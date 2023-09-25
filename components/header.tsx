e7ec5508a424c0c0e81a6614bded87362f740cd0
Łukasz <lukasz.poczta12@gmail.com>
2023-09-25 12:11:34 +0200

    added header

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
+import { links } from '@/lib/data';
+import Link from 'next/link';
 
 const Header = () => {
   return (
@@ -9,7 +11,25 @@ const Header = () => {
       <motion.div
         className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-r-8 border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
         initial={{ opacity: 0, y: -100, x: '-50%' }}
-        animate={{ opacity: 1, y: 0, x: '-50%' }}></motion.div>
+        animate={{ opacity: 1, y: 0, x: '-50%' }}
+        ></motion.div>
+      <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5 '>
+          {links.map((link) => (
+            <motion.li
+              key={link.hash}
+              className='h-3/4 flex items-center justify-center'
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0 }}>
+              <Link
+                href={link.hash}
+                className='flex justify-center items-center px-3 py-3 hover:text-gray-950 transition'>
+                {link.name}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
